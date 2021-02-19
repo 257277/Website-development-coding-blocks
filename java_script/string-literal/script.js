5546d1705d7aeb2079499b0bd0fc395090816484
Rahul sharma <76rahul257@gmail.com>
2021-02-19 17:15:49 +0530

    adding string literal

---
+++ java_script/string-literal/script.js
@@ -0,0 +1,8 @@
+// He said,"this is awesome!"
+// If we want to print the above live as it is we have following methods
+
+let s1 = 'He said,"This is awesome!"';  
+
+let s2 = "He said ,\"This is awesome!\"";
+let s3=`He said,"this is awesome!`;
+let s4= "This sentence is \n in multiple line";
