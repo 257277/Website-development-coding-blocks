9cb2238843cbc629ac2e8073b039c6ab7c0d7d49
Rahul sharma <76rahul257@gmail.com>
2021-02-19 17:20:14 +0530

    adding changes in string literal

--- java_script/string-literal/script.js
+++ java_script/string-literal/script.js
@@ -1,6 +1,6 @@
 // He said,"this is awesome!"
 // If we want to print the above live as it is we have following methods
-
+// $ is only work with `
 let s1 = 'He said,"This is awesome!"';  
 
 let s2 = "He said ,\"This is awesome!\"";
