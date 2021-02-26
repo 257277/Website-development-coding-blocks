ec320aa7a8369ae954c76ec4b0f8f0bb04e01187
Rahul sharma <76rahul257@gmail.com>
2021-02-26 13:59:00 +0530

    Adding ES6 Call Stack

---
+++ java_script/ES6/Call Stack/script.js
@@ -0,0 +1,29 @@
+function ceo()
+{
+    console.log.log("Let's make a dent in the universe");
+    cto();
+}
+
+function cto()
+{
+    console.log("Let's make a game changing product");
+    vpengg();
+}
+
+function vpengg()
+{
+    console.log("Let's make a new javascript framework");
+    techlead();
+}
+function techlead()
+{
+    console.log("Let's port Angular to Typescript");
+    developer();
+}
+function developer()
+{
+    console.log("Let's copy some code from StackOverFlow");
+    throw new Error("The code didn't work");
+}
+
+ceo();
