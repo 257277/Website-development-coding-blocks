01bcecb93941c3e4f7878a7a26581e2a8753b7b9
Rahul sharma <76rahul257@gmail.com>
2021-05-16 22:49:41 +0530

    Adding mysql express server and api

--- mysql_express_server/server.js
+++ mysql_express_server/server.js
@@ -1,46 +1,18 @@
-const express =require('express')
-const app= express();
-const db=require('./db')
+
+const express = require('express')
+const app = express()
+const path = require('path')
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({extended: true}))
 
-app.set("view engine","hbs")
+app.set("view engine", "hbs")
 
-
-app.get('/',(req,res)=>
-{
-db.getAllPersons()
-.then((persons)=>
-{
-    res.render('persons',{persons})
-})
-.catch((err)=>
-{
-res.send(err)
-})
-})
-
-    
-
-app.get('/add',(req,res)=>
-{
-    res.render('persons_add')
-})
-app.post('/add',(req,res)=>
-{
-db.addNewPerson(req.body.name,req.body.age,req.body.city)
-.then(()=>
-{
-    res.redirect('/')
-})
-.catch((err)=>
-{
-    res.send(err)
-})
-})
+app.use('/pages', require('./routes/pages').route)
+app.use('/api', require('./routes/api').route)
+app.use('/', express.static(path.join(__dirname, 'public_static')))
 
 
-app.listen(4444,()=>{
-console.log("Server is started on http://localhost:4444/")
+app.listen(4444, () => {
+    console.log("Server started on http://localhost:4444/")
 })
